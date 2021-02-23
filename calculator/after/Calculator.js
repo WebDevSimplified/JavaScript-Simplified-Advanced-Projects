97d7b4541a3efaf6aa3669bf1d9aa6508eb906e6
Web Dev Simplified <webdevsimplified@gmail.com>
2021-02-23 12:10:28 -0600

    Finish Video

--- calculator/after/Calculator.js
+++ calculator/after/Calculator.js
@@ -1,8 +1,4 @@
 export default class Calculator {
-  #primaryOperandDisplay
-  #secondaryOperandDisplay
-  #operationDisplay
-
   constructor(
     primaryOperandDisplay,
     secondaryOperandDisplay,
@@ -15,6 +11,36 @@ export default class Calculator {
     this.clear()
   }
 
+  #primaryOperandDisplay
+  #secondaryOperandDisplay
+  #operationDisplay
+
+  get primaryOperand() {
+    return parseFloat(this.#primaryOperandDisplay.dataset.value)
+  }
+
+  set primaryOperand(value) {
+    this.#primaryOperandDisplay.dataset.value = value ?? ""
+    this.#primaryOperandDisplay.textContent = displayNumber(value)
+  }
+
+  get secondaryOperand() {
+    return parseFloat(this.#secondaryOperandDisplay.dataset.value)
+  }
+
+  set secondaryOperand(value) {
+    this.#secondaryOperandDisplay.dataset.value = value ?? ""
+    this.#secondaryOperandDisplay.textContent = displayNumber(value)
+  }
+
+  get operation() {
+    return this.#operationDisplay.textContent
+  }
+
+  set operation(value) {
+    this.#operationDisplay.textContent = value ?? ""
+  }
+
   addDigit(digit) {
     if (
       digit === "." &&
@@ -23,97 +49,68 @@ export default class Calculator {
       return
     }
     if (this.primaryOperand === 0) {
-      this.#primaryOperand = digit
+      this.primaryOperand = digit
       return
     }
-    this.#primaryOperand = this.#primaryOperandDisplay.dataset.value + digit
+    this.primaryOperand = this.#primaryOperandDisplay.dataset.value + digit
   }
 
   removeDigit() {
     const numberString = this.#primaryOperandDisplay.dataset.value
     if (numberString.length <= 1) {
-      this.#primaryOperand = 0
+      this.primaryOperand = 0
       return
     }
-    this.#primaryOperand = numberString.substring(0, numberString.length - 1)
-  }
 
-  chooseOperation(operation) {
-    this.#operation = operation
-    this.#secondaryOperand = this.primaryOperand
-    this.#primaryOperand = 0
+    this.primaryOperand = numberString.substring(0, numberString.length - 1)
   }
 
   evaluate() {
     let result
     switch (this.operation) {
-      case "+":
-        result = this.secondaryOperand + this.primaryOperand
-        break
-      case "-":
-        result = this.secondaryOperand - this.primaryOperand
-        break
       case "*":
         result = this.secondaryOperand * this.primaryOperand
         break
       case "÷":
         result = this.secondaryOperand / this.primaryOperand
         break
+      case "+":
+        result = this.secondaryOperand + this.primaryOperand
+        break
+      case "-":
+        result = this.secondaryOperand - this.primaryOperand
+        break
       default:
         return
     }
 
     this.clear()
-    this.#primaryOperand = result
+    this.primaryOperand = result
 
     return result
   }
 
+  chooseOperation(operation) {
+    if (this.operation !== "") return
+    this.operation = operation
+    this.secondaryOperand = this.primaryOperand
+    this.primaryOperand = 0
+  }
+
   clear() {
-    this.#primaryOperand = 0
-    this.#secondaryOperand = null
-    this.#operation = null
-  }
-
-  get primaryOperand() {
-    return parseFloat(this.#primaryOperandDisplay.dataset.value)
-  }
-
-  set #primaryOperand(value) {
-    this.#primaryOperandDisplay.dataset.value = value
-    this.#primaryOperandDisplay.textContent = displayNumber(value)
-  }
-
-  get secondaryOperand() {
-    return parseFloat(this.#secondaryOperandDisplay.dataset.value)
-  }
-
-  set #secondaryOperand(value) {
-    this.#secondaryOperandDisplay.dataset.value = value
-    this.#secondaryOperandDisplay.textContent = displayNumber(value)
-  }
-
-  get operation() {
-    return this.#operationDisplay.textContent
-  }
-
-  set #operation(value) {
-    this.#operationDisplay.textContent = value ?? ""
+    this.primaryOperand = 0
+    this.secondaryOperand = null
+    this.operation = null
   }
 }
 
-const NUMBER_FORMATTER = new Intl.NumberFormat("en", {
-  maximumFractionDigits: 20,
-})
+const NUMBER_FORMATTER = new Intl.NumberFormat("en")
 
-function displayNumber(value) {
-  value = value?.toString() ?? ""
-  if (value === "") return ""
-
-  const [integer, decimal] = value.split(".")
+function displayNumber(number) {
+  const stringNumber = number?.toString() || ""
+  if (stringNumber === "") return ""
+  const [integer, decimal] = stringNumber.split(".")
   const formattedInteger = NUMBER_FORMATTER.format(integer)
-  if (decimal == null) {
-    return formattedInteger
-  }
+  if (decimal == null) return formattedInteger
   return formattedInteger + "." + decimal
 }
--- calculator/after/script.js
+++ calculator/after/script.js
@@ -13,19 +13,19 @@ const calculator = new Calculator(
 )
 
 document.addEventListener("click", e => {
+  if (e.target.matches("[data-all-clear]")) {
+    calculator.clear()
+  }
   if (e.target.matches("[data-number]")) {
     calculator.addDigit(e.target.textContent)
+  }
+  if (e.target.matches("[data-delete]")) {
+    calculator.removeDigit()
   }
   if (e.target.matches("[data-operation]")) {
     calculator.chooseOperation(e.target.textContent)
   }
   if (e.target.matches("[data-equals]")) {
     calculator.evaluate()
-  }
-  if (e.target.matches("[data-all-clear]")) {
-    calculator.clear()
-  }
-  if (e.target.matches("[data-delete]")) {
-    calculator.removeDigit()
   }
 })
